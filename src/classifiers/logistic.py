eeb62a4d158609d62ea14173d3b7587138f3f016
xuedong <shang.xuedong@yahoo.fr>
2018-01-25 19:41:23 +0100

    Imlementing logistic class.

--- src/classifiers/logistic.py
+++ src/classifiers/logistic.py
@@ -13,7 +13,7 @@ import theano.tensor as T
 
 class LogisticRegression(object):
     def __init__(self, input, n, m):
-        """
+        """Initialization of the class.
 
         :param input: one minibatch
         :param n: the dimension of the input space
@@ -37,3 +37,16 @@ class LogisticRegression(object):
             name='b',
             borrow=True
         )
+        # compute the matrix of class-membership probabilities
+        self.p_y_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
+        # predict the class
+        self.y_pred = T.argmax(self.p_y_x, axis=1)
+
+    def neg_log_likelihood(self, y):
+        """Loss function.
+
+        :param y: the correct label vector
+        :return: the mean of the negative log-likelihood of the prediction, we use mean instead of sum here
+        to make the learning rate less dependent of the size of the minibatch size
+        """
+        return -T.mean(T.log(self.p_y_x)[T.arange(y.shape[0]), y])
