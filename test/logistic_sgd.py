6ada2f2e9f503af98e68b2a61f875b1f58bffc3e
xuedong <shang.xuedong@yahoo.fr>
2018-01-29 19:46:43 +0100

    Debugging logistic_sgd.

--- test/logistic_sgd.py
+++ test/logistic_sgd.py
@@ -3,6 +3,8 @@ import timeit
 import theano
 import theano.tensor as T
 
+from six.moves import cPickle
+
 import sys
 sys.path.insert(0, '../src/classifiers')
 sys.path.insert(0, '../src')
@@ -86,6 +88,84 @@ def sgd(dataset, learning_rate, epochs, batch_size):
 
     print('Training model...')
 
+    # early-stopping parameters
+    patience = 5000
+    patience_increase = 2
+    threshold = 0.995
+    valid_freq = min(n_batches_train, patience // 2)
 
+    best_valid_loss = np.inf
+    test_score = 0.
+    start_time = timeit.default_timer()
+
+    done = False
+    epoch = 0
+    while (epoch < epochs) and not done:
+        epoch += 1
+        for batch_index in range(n_batches_train):
+            batch_cost = train_model(batch_index)
+            iter = (epoch - 1) * n_batches_train + batch_index
+
+            if (iter + 1) % valid_freq == 0:
+                valid_losses = [valid_model(i) for i in range(n_batches_valid)]
+                current_valid_loss = np.mean(valid_losses)
+
+                print(
+                    'epoch %i, batch %i/%i, batch average cost %f, validation error %f %%' %
+                    (
+                        epoch,
+                        batch_index + 1,
+                        n_batches_train,
+                        batch_cost,
+                        current_valid_loss * 100.)
+                )
+
+                if current_valid_loss < best_valid_loss:
+                    if current_valid_loss < best_valid_loss * threshold:
+                        patience = max(patience, iter * patience_increase)
+
+                    best_valid_loss = current_valid_loss
+
+                    test_losses = [test_model(i) for i in range(n_batches_test)]
+                    test_score = np.mean(test_losses)
+
+                    print(
+                        (
+                            '     epoch %i, batch %i/%i, test error of'
+                            ' best model %f %%'
+                        ) %
+                        (
+                            epoch,
+                            batch_index + 1,
+                            n_batches_train,
+                            test_score * 100.
+                        )
+                    )
+
+                    # save the best model
+                    with open('best_model_logistic_sgd.pkl', 'wb') as file:
+                        cPickle.dump(classifier, file)
+
+            if patience <= iter:
+                done = True
+                break
+
+    end_time = timeit.default_timer()
+    print(
+        (
+            'Optimization completed with best validation score of %f %%,'
+            'with test performance %f %%'
+        )
+        % (best_valid_loss * 100., test_score * 100.)
+    )
+    print('The code run for %d epochs, with %f epochs/sec' % (
+        epoch, 1. * epoch / (end_time - start_time)))
+    print(('The code for file ' +
+           os.path.split(__file__)[1] +
+           ' ran for %.1fs' % ((end_time - start_time))), file=sys.stderr)
 
     return ()
+
+
+if __name__=='__main__':
+    sgd(DATASET, LEARNING_RATE, EPOCHS, BATCH_SIZE)
