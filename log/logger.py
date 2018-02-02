af9d73237b7f499a66623540f619510f95923008
xuedong <shang.xuedong@yahoo.fr>
2018-02-02 13:22:32 +0100

    Finished the logger class.

--- log/logger.py
+++ log/logger.py
@@ -1,2 +1,17 @@
+import sys
+
+
 class Logger(object):
-    
+    """Write messages at the same time in a file and in the terminal"""
+    def __init__(self, path):
+        """
+
+        :param path: path to the place where we log messages
+        """
+        self.terminal = sys.stdout
+        self.log = open(path + '/hyperband_run.log', 'a')
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+        self.log.flush()
