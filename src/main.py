396f00b095c5c9b0c1d43fa8f7e3e24ca7e22b37
xuedong <shang.xuedong@yahoo.fr>
2018-02-26 07:19:30 +0100

    Debugging...

--- src/main.py
+++ src/main.py
@@ -3,14 +3,14 @@ import sys
 import os
 import theano.tensor as ts
 
-import logger
+# import logger
 import utils
 import logistic
 import hyperband_finite
 from params import Param
 
 
-def get_cnn_search_space():
+def get_search_space():
     params = {}
     params['learning_rate'] = Param('learning_rate', numpy.log(1*10**(-3)), numpy.log(1*10**(-1)), dist='uniform', scale='log')
     params['batch_size'] = Param('batch_size', 1, 1000, dist='uniform', scale='linear', interval=1)
@@ -30,7 +30,7 @@ def main():
     data = utils.load_data(data_dir)
     x = ts.matrix('x')
     model = logistic.LogisticRegression(x, 28*28, 10)
-    params = get_cnn_search_space()
+    params = get_search_space()
     # arms = model.generate_arms(1, "../result/", params, True)
     # train_loss, val_acc, test_acc = logistic.run_solver(1000, arms[0], data)
     hyperband_finite.hyperband_finite(model, 'epoch', params, 1, 1000, 360, director, data)
