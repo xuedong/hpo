c0dc609cf2ba2856ab2929906d99f9b9c924a2b1
Xuedong Shang <shang.xuedong@yahoo.fr>
2018-03-12 13:25:00 +0100

    Added a plot_all function.

--- src/plots.py
+++ src/plots.py
@@ -114,3 +114,79 @@ def plot_tpe(path, runs, classifier_name, optimizer_name, dataset_name, idx):
     plt.savefig(os.path.join(os.path.abspath('../../'), 'img/{}/{}.pdf'.format('tpe_' + classifier_name +
                                                                                str(idx), dataset_name)))
     plt.close(fig)
+
+
+def plot_all(path1, path2, s_max, runs, classifier_name, optimizer_name, dataset_name, idx):
+    os.chdir(path1)
+    fig = plt.figure()
+
+    # Hyperband brackets
+    longest = 0
+    for s in range(s_max + 1):
+        tracks = np.array([None for _ in range(runs)])
+        shortest = sys.maxsize
+        # compute the length of the shortest test error vector
+        for i in range(runs):
+            [_, _, track] = cPickle.load(open(classifier_name + optimizer_name + str(i)
+                                              + '/results_' + str(s) + '.pkl', 'rb'))
+            if len(track) < shortest:
+                shortest = len(track)
+        for i in range(runs):
+            [_, _, track] = cPickle.load(open(classifier_name + optimizer_name + str(i)
+                                              + '/results_' + str(s) + '.pkl', 'rb'))
+            # truncate each test error vector by the length of the shortest one
+            tracks[i] = track[0:shortest]
+
+        # compute the longest truncated test error vector among all s values
+        length = len(tracks[0])
+        if length > longest:
+            longest = length
+        x = range(length)
+        y = np.mean(tracks, axis=0)
+        if s == 0:
+            plt.plot(x, y, label=r"Random Search")
+        else:
+            plt.plot(x, y, label=r"$\mathtt{s=}$" + str(s))
+
+    # Hyperband
+    tracks = np.array([None for _ in range(runs)])
+    for i in range(runs):
+        [_, _, track] = cPickle.load(open(classifier_name + optimizer_name + str(i) + '/results.pkl', 'rb'))
+        tracks[i] = track[0:longest]
+
+    # length = len(tracks[0])
+    x = range(longest)
+    y = np.mean(tracks, axis=0)
+    plt.plot(x, y, label=r"Hyperband")
+
+    # TPE
+    os.chdir(path2)
+    shortest = sys.maxsize
+
+    tracks = np.array([None for _ in range(runs)])
+    for i in range(runs):
+        [trials, _] = cPickle.load(open(classifier_name + optimizer_name + str(i) + '/results.pkl', 'rb'))
+        track = combine_tracks(trials)
+        if len(track) < shortest:
+            shortest = len(track)
+    for i in range(runs):
+        [trials, _] = cPickle.load(open(classifier_name + optimizer_name + str(i) + '/results.pkl', 'rb'))
+        track = combine_tracks(trials)
+        tracks[i] = track[0:shortest]
+
+    # length = len(tracks[0])
+    x = range(shortest)
+    y = np.mean(tracks, axis=0)
+    plt.plot(x, y, label=r"TPE")
+
+    plt.grid()
+    plt.ylim((0, 0.2))
+    plt.legend(loc=0)
+    plt.ylabel('Test Error')
+    plt.xlabel('Number of Epochs')
+    save_path = os.path.join(os.path.abspath('../../'), 'img/{}'.format(classifier_name + str(idx)))
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    plt.savefig(os.path.join(os.path.abspath('../../'), 'img/{}/{}.pdf'.format(classifier_name +
+                                                                               str(idx), dataset_name)))
+    plt.close(fig)
--- test/plots_test.py
+++ test/plots_test.py
@@ -9,5 +9,9 @@ if __name__ == "__main__":
 
     # path = "../result/tpe_logistic_0/"
     # plots.plot_tpe(path, 10, 'logistic_', 'sgd_', 'mnist', 0)
-    path = "../result/tpe_mlp_0"
-    plots.plot_tpe(path, 5, 'mlp_', 'sgd_', 'mnist', 0)
+    # path = "../result/tpe_mlp_0"
+    # plots.plot_tpe(path, 5, 'mlp_', 'sgd_', 'mnist', 0)
+
+    path1 = "../result/hyperband_mlp_0"
+    path2 = "../tpe_mlp_0"
+    plots.plot_all(path1, path2, 3, 5, 'mlp_', 'sgd_', 'mnist', 0)
